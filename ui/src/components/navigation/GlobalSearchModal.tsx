1c3366f2e0e22fc650df6a6fd23781a8a584028a
JoshNg123 <jnjw,999@gmail.com>
2025-09-21 15:37:04 +0800

    GH-178: Minor Fix for Global Search Modal

--- ui/src/components/navigation/GlobalSearchModal.tsx
+++ ui/src/components/navigation/GlobalSearchModal.tsx
@@ -251,7 +251,7 @@ const GlobalSearchModal = (props: GlobalSearchModalProps) => {
           sx={{
             position: 'fixed',
             top: ['0vh', '22.5vh'],
-            height: { xs: `calc(100vh - ${MOBILE_NAVBAR_HEIGHT})`, sm: 'auto' },
+            height: { sm: 'auto' },
             left: ['0vw', '30vw'],
             backgroundColor: '#171717',
             borderRadius: '10px',
