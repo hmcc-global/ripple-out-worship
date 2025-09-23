79915d31e63cd0d30494a70a36ac3141507bba2a
unknown <ricia1212@gmail.com>
2025-09-24 01:03:36 +0800

    GH-185: update the theme constants

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -57,11 +57,29 @@ export const timeSignatureOptions = ['4/4', '3/4', '6/8', '2/4', '2/2', '9/8', '
 
 export const themeOptions = [
   'Attributes of God',
-  'Declaration',
+  'Declaration (Victorious/Kingdom)',
   'The Cross',
   'Call to Worship',
   'Celebration',
-  'Church/Unity',
+  'Church / Unity',
+  'Dependence',
+  'Freedom / Overcoming Sin',
+  `God's Faithfulness`,
+  `God's Presence`,
+  'Holiness / Purity',
+  'Holy Spirit',
+  'Hope',
+  'Intimacy / Love for God',
+  'Missions / Evangelism',
+  'Obedience / Trust',
+  'Prayer',
+  'Purpose/Destiny',
+  'Repentance',
+  'Resurrection',
+  'Surrender / Sacrifice',
+  'Thankfulness/Gratitude',
+  'The Cross',
+  `Worship / Adoration / Praise`,
 ];
 export const themeSelectionLimit = 3;
 
