d9e12e8959c26336f6ea941a80de881339f8d596
unknown <ricia1212@gmail.com>
2025-09-27 02:56:12 +0800

    GH-185: make it mobile friendly

--- ui/src/components/song/SongEditorContainer.tsx
+++ ui/src/components/song/SongEditorContainer.tsx
@@ -193,51 +193,70 @@ const SongEditorContainer: FC<SongEditorProps> = () => {
 
   const SongActionButtons = ({ display }: { display: boolean }) => {
     return (
-      <Stack direction="row" display={display ? 'flex' : 'none'}>
-        {action === 'edit' ? (
-          <>
-            <Button
-              color="error"
-              variant="outlined"
-              startIcon={<DeleteIcon />}
-              onClick={() => setOpenDeleteDialog(true)}
-              sx={{
-                textTransform: 'none',
-                borderRadius: '100px',
-                px: 3,
-              }}
-            >
-              Delete
-            </Button>
-            <Divider variant="fullWidth" orientation="vertical" flexItem sx={{ mx: 2 }} />
-          </>
+      <Box>
+        <Stack direction="row" display={display ? 'flex' : 'none'}>
+          {action === 'edit' && isDesktop ? (
+            <>
+              <Button
+                color="error"
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+                onClick={() => setOpenDeleteDialog(true)}
+                sx={{
+                  textTransform: 'none',
+                  borderRadius: '100px',
+                  px: 3,
+                }}
+              >
+                Delete
+              </Button>
+              <Divider variant="fullWidth" orientation="vertical" flexItem sx={{ mx: 2 }} />
+            </>
+          ) : null}
+          <Button
+            type={'submit'}
+            color="secondary"
+            variant="contained"
+            sx={{
+              mr: 1,
+              textTransform: 'none',
+              borderRadius: '100px',
+              px: 3,
+            }}
+          >
+            Save
+          </Button>
+          <Button
+            color={'secondary'}
+            sx={{
+              textTransform: 'none',
+              borderRadius: '100px',
+              border: 1,
+              px: 2,
+            }}
+            onClick={() => navigate('/song')}
+          >
+            Cancel
+          </Button>
+        </Stack>
+        {action === 'edit' && !isDesktop ? (
+          <Button
+            color="error"
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+            onClick={() => setOpenDeleteDialog(true)}
+            sx={{
+              textTransform: 'none',
+              borderRadius: '100px',
+              px: 3,
+              py: '6px',
+              mt: 2,
+            }}
+          >
+            Delete
+          </Button>
         ) : null}
-        <Button
-          type={'submit'}
-          color="secondary"
-          variant="contained"
-          sx={{
-            mr: 1,
-            textTransform: 'none',
-            borderRadius: '100px',
-            px: 3,
-          }}
-        >
-          Save
-        </Button>
-        <Button
-          color={'secondary'}
-          sx={{
-            textTransform: 'none',
-            borderRadius: '100px',
-            border: 1,
-            px: 2,
-          }}
-          onClick={() => navigate('/song')}
-        >
-          Cancel
-        </Button>
-      </Stack>
+      </Box>
     );
   };
 
