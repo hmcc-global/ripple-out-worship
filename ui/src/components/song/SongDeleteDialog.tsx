e526397b02c9ac59a980400f7a467749d554402d
unknown <ricia1212@gmail.com>
2025-09-27 02:41:12 +0800

    GH-185: add delete song button function

---
+++ ui/src/components/song/SongDeleteDialog.tsx
@@ -0,0 +1,51 @@
+import { Box, Stack, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ConfirmationDialog from '../custom/ConfirmationDialog';
+import { customAxios as axios } from '../custom/customAxios';
+import { useNavigate } from 'react-router-dom';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  songId?: string;
+};
+const SongDeleteDialog = ({ open, onClose, songId }: Props) => {
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      await axios.put('/api/songs/delete', { id: songId });
+    } catch (error) {
+      console.log(error);
+    }
+    onClose();
+    navigate('/song');
+  };
+
+  return (
+    <ConfirmationDialog
+      open={open}
+      onClose={onClose}
+      onConfirm={handleDelete}
+      title={
+        <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+          <DeleteIcon color="error" fontSize="large" />
+          <Typography variant="h3" color="error">
+            Delete Song
+          </Typography>
+        </Box>
+      }
+      confirmText="Confirm Delete Song"
+      confirmColor="error"
+      cancelColor="error"
+      showCloseIcon
+    >
+      <Stack spacing={2} alignItems="center" textAlign="center" paddingY={2}>
+        <Typography variant="h5">Are you sure you want to delete this song?</Typography>
+        <Typography>This action cannot be undone</Typography>
+      </Stack>
+    </ConfirmationDialog>
+  );
+};
+
+export default SongDeleteDialog;
