2c53777acf159164057df51552738a1075b0cff3
Midnite1212 <richie.salim@bowtie.com.hk>
2025-12-19 02:07:55 +0800

    GH-196: force auto refresh after creating or updating setlist

--- ui/src/components/setlist/SetlistEditorContainer.tsx
+++ ui/src/components/setlist/SetlistEditorContainer.tsx
@@ -632,9 +632,12 @@ const SetlistEditorContainer: FC<SetlistEditorProps> = () => {
       // Success - clear errors and redirect
       setInvalidSetlist('');
       setSuccessSnackbarOpen(true);
-      navigate(
-        isTablet || isDesktop ? `/setlist/${savedSetlistId}` : `/setlist/details/${savedSetlistId}`
-      );
+      //TODO: Find a better way to redirect to list and refetch the newly created/updated setlist.
+      if (isTablet || isDesktop) {
+        window.location.href = `/setlist/${savedSetlistId}`;
+      } else {
+        window.location.href = `/setlist/details/${savedSetlistId}`;
+      }
     } catch (error: any) {
       console.error('Error saving setlist:', error);
       setInvalidSetlist(error.response?.data || 'Error saving setlist');
